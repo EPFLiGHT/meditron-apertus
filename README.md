851eba9da88d78ce32595697ef5c54a2ce3362bc
Xkrilandar <xavier.theimerlienhard@gmail.com>
2025-12-07 17:49:54 +0100

    debug training

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 ## But FIRST:
 - create a .env using .env.example as an example
-- git clone axolotl
-- git clone our fork of lm harness
-- make sure your environment has the necessary requirements
+- git clone a fork of lm harness or lm harness (https://github.com/Xkrilandar/lm-evaluation-harness)
+- make sure your environment has the necessary requirements 
--- run_eval_data_parallelism.sh
+++ run_eval_data_parallelism.sh
@@ -1,18 +1,21 @@
 if [ -f .env ]; then set -a; source .env; set +a; fi
 
-cd "$PROJECT_ROOT/lm-evaluation-harness"
+cd "/users/theimer/lm-evaluation-harness"
 pip install -e .
 pip install git+https://github.com/huggingface/datasets.git
 
-cd "$PROJECT_ROOT/lm-evaluation-harness/lm_eval/tasks"
+cd "/users/theimer/lm-evaluation-harness/lm_eval/tasks"
 
 export WORLD_SIZE=$SLURM_NNODES
 export MASTER_ADDR=$(hostname)
 export MASTER_PORT=6300
 export HF_HOME=$USER_STORAGE/hf
+export HF_DATASETS_TRUST_REMOTE_CODE=1
+export TRITON_CACHE_DIR=$USER_STORAGE/triton
+export HF_DATASETS_CACHE=$HF_HOME/datasets
+export TRANSFORMERS_CACHE=$HF_HOME/transformers
 
-
-export MODEL_PATH="Qwen/Qwen3-32B"
+export MODEL_PATH="/capstor/store/cscs/swissai/a127/apertus/huggingface/Apertus8B"
 
 echo "WORLD_SIZE=$WORLD_SIZE"
 echo "MASTER_ADDR=$MASTER_ADDR"
